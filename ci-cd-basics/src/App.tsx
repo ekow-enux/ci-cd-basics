8628f8fa67e388a0f49485b6207c1723d70432f8
ENOCK YAW ENU <enochenux@gmail.com>
2025-12-09 15:44:31 +0000

    removed uncessary code

--- ci-cd-basics/src/App.tsx
+++ ci-cd-basics/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 
 function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
+
   const [activeSection, setActiveSection] = useState('hero');
 
   // Smooth scroll to section
